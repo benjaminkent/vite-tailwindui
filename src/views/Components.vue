00c2f52067f0bd1deaa8a7df1d4eb98f5d91355d
Ben Jehl <ben.jehl@bill360.com>
2022-02-17 10:05:45 -0500

    base component page layout

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,43 +1,19 @@
 <template>
   <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
     <li
-      v-for="person in people"
-      :key="person.email"
-      class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
+      v-for="(component, index) in components"
+      :key="index"
+      class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 flex flex-col items-center py-5"
     >
-      foo stuff in here
+      <h1 class="text-xl font-bold mb-5">{{ component.name }}</h1>
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
-const people = [
+const components = [
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    name: 'Guage',
   },
 ]
 </script>

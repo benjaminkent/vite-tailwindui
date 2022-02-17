a77ac72b395d548f9cfd8c13e2dad7def843e789
Ben Jehl <ben.jehl@bill360.com>
2022-02-17 10:15:19 -0500

    add gauge component

---
+++ src/components/Gauge.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>the gauge component</div>
+</template>
+
+<script setup lang="ts"></script>
--- src/views/Components.vue
+++ src/views/Components.vue
@@ -3,17 +3,21 @@
     <li
       v-for="(component, index) in components"
       :key="index"
-      class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 flex flex-col items-center py-5"
+      class="col-span-1 bg-white rounded-lg shadow flex flex-col items-center py-5"
     >
       <h1 class="text-xl font-bold mb-5">{{ component.name }}</h1>
+      <component :is="component.component" />
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
+import Gauge from '@components/Gauge.vue'
+
 const components = [
   {
-    name: 'Guage',
+    name: 'Gauge',
+    component: Gauge,
   },
 ]
 </script>

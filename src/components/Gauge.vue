40f9c6891df44856d9dca88f6fee35c69447e5e1
Ben Jehl <ben.jehl@bill360.com>
2022-02-17 11:48:45 -0500

    set color and animate on mount

--- src/components/Gauge.vue
+++ src/components/Gauge.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="progress my-5">
     <div class="barOverflow">
-      <div class="bar"></div>
+      <div
+        class="bar"
+        :style="`transform: rotate(${45 + progress * 1.8}deg);`"
+      ></div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const progress = ref(0)
+
+function setProgress(p: number) {
+  progress.value = p
+}
+
+onMounted(() => {
+  setTimeout(() => {
+    setProgress(80)
+  }, 1)
+})
+</script>
 
 <style scoped lang="scss">
 .progress {
@@ -15,24 +32,22 @@
   text-align: center;
 }
 .barOverflow {
-  /* Wraps the rotating .bar */
   position: relative;
-  overflow: hidden; /* Comment this line to understand the trick */
+  overflow: hidden;
   width: 272px;
-  height: 130px; /* Half circle (overflow) */
-  margin-bottom: -14px; /* bring the numbers up */
+  height: 130px;
 }
 .bar {
   position: absolute;
   top: 0;
   left: 0;
   width: 272px;
-  height: 272px; /* full circle! */
+  height: 272px;
   border-radius: 50%;
   box-sizing: border-box;
-  border: 70px solid #eee; /* half gray, */
-  border-bottom-color: #0bf; /* half azure */
-  border-right-color: #0bf;
-  transform: rotate(45deg);
+  border: 70px solid #eee;
+  border-bottom-color: #6b9f00;
+  border-right-color: #6b9f00;
+  transition: all 1s ease-in-out;
 }
 </style>
